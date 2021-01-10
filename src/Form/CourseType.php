0d7dc84387e99bd68b3c62930f48ef37a98278e3
Xavier-alaji <xavier.gerard.mail@gmail.com>
2021-01-10 02:33:45 +0100

    creud Course

---
+++ src/Form/CourseType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('type')
+            ->add('sss')
+            ->add('slope')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('created_by')
+            ->add('updated_by')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
